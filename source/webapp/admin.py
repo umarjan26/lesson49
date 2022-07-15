64764d8a40b6ad37c20a24b115a5cfc0f0af9e07
umar <umarjan260797@gmail.com>
2022-07-15 11:51:14 +0300

    зарегистрировали админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Task, Status, Type
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type', 'description']
+    list_display_links = ['summary']
+    list_filter = ['summary', 'type']
+    search_fields = ['summary']
+    fields = ['summary', 'status', 'type', 'description', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['name', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['name', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Type, TypeAdmin)
