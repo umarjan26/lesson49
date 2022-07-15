51b1f57479ce9bf9d9132bef8c097b9fcb1164f2
umar <umarjan260797@gmail.com>
2022-07-15 16:59:21 +0300

    сделали обновление страницу

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView, RedirectView
 
-from webapp.views import IndexView, Create, ArticleView
+from webapp.views import IndexView, Create, ArticleView, Update
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('add/', Create.as_view(), name="add"),
-    path('task/<int:pk>/', ArticleView.as_view(), name="view")
+    path('task/<int:pk>/', ArticleView.as_view(), name="view"),
+    path('task/<int:pk>/update/', Update.as_view(), name="update")
 
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,8 @@
+from audioop import reverse
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 
 from webapp.forms import TaskForm
 from webapp.models import Task
@@ -40,3 +42,29 @@ class Create(View):
             return redirect("view", pk=new_task.pk)
         return render("create.html", {"form": form})
 
+
+
+class Update(View):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        tasks = get_object_or_404(Task, pk=pk)
+        form = TaskForm(initial={
+            "summary": tasks.summary,
+            "status": tasks.status,
+            "description": tasks.description,
+            "type": tasks.type
+        })
+        return render(request, "update.html", {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        tasks = get_object_or_404(Task, pk=pk)
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            tasks.summary = form.cleaned_data.get("summary")
+            tasks.status = form.cleaned_data.get("status")
+            tasks.type = form.cleaned_data.get("type")
+            tasks.description = form.cleaned_data.get("description")
+            tasks.save()
+            return redirect("view", pk=tasks.pk)
+        return render(request, "update.html", {'form': form})
